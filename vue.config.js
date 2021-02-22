9996ae1d6519de791583a317a12d4a19dc297384
lautarodapin <lautarodapin@gmail.com>
2021-02-22 19:42:59 -0300

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
-    publicPath: "http://127.0.0.1:8000/",
+    publicPath: "http://0.0.0.0:8000/",
     assetsDir: 'static',
     outputDir: './dist/',
 
